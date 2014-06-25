7ecf32018dc0d9fd54287eebc4ca00da7e24fbb9
Sara Alspaugh <saraalspaugh@gmail.com>
2014-06-25 13:28:48 -0700

    Added cleaned up script to make Figure 2.

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ toppaths:
 	echo "Outputting data for top paths in text on page 9."
 
 fig2:
-	echo "Making Figure 2."
+	python categories/histogram.py -s postgresdb -U lupe -P lupe -D lupe
 
 fig3:
 	echo "Making Figure 3."
@@ -24,13 +24,10 @@ fig5:
 	echo "Making Figure 5."
 
 fig6:
-	echo "Making Figure 6."
 	python clustering/main.py -s postgresdb -U lupe -P lupe -D lupe -p 9 -f filters01 -l filters -n -d 10 -o results/fig6.clusters -u results/fig6.mouseovers -t results/fig6.features
 
 fig7:
-	echo "Making Figure 7."
 	python clustering/main.py -s postgresdb -U lupe -P lupe -D lupe -p 9 -f augments01 -l augments -n -d 2 -o results/fig7.clusters -u results/fig7.mouseovers -t results/fig7.features
 
 fig8:
-	echo "Making Figure 8."
 	python clustering/main.py -s postgresdb -U lupe -P lupe -D lupe -p 9 -f aggregates01 -l aggregates -n -d 100 -o results/fig8.clusters -u results/fig8.mouseovers -t results/fig8.features
--- categories/histogram.py
+++ categories/histogram.py
@@ -1,64 +1,60 @@
-
-from argparse import ArgumentParser
+from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
-
-import helpers.db as dbhelper
-import helpers.commands as commhelper
-import helpers.queries as qhelper
+from queryutils.parse import tokenize_query
+from queryutils.datasource import CSVFiles, JSONFiles, PostgresDB, SQLite3DB
+from queryutils.splunktypes import lookup_category
 
 RESULTS_DIR = "results/"
-TYPE = 'all'
 
-total_queries = 0
+SOURCES = {
+    "csvfiles": (CSVFiles, ["srcpath", "version"]),
+    "jsonfiles": (JSONFiles, ["srcpath", "version"]),
+    "postgresdb": (PostgresDB, ["database", "user", "password"]),
+    "sqlite3db": (SQLite3DB, ["srcpath"])
+}
 
+def main(source):
+    category_counts, nqueries = tally_category_counts(source)
+    create_histogram(category_counts, nqueries)
 
-def main(textlist):
-    db = dbhelper.connect_db()
-    querydata = dbhelper.get_data(db, TYPE)
-    commands = commhelper.get_commands(textlist)
-    categories, missing = process(querydata, commands)
-    create_histogram(categories)
-    write_missing(missing)
+def tally_category_counts(source):
+    category_counts = defaultdict(int)
+    nqueries = 0
+    for query in source.get_queries():
+        categories = lookup_categories(query)
+        if len(categories) > 0:
+            nqueries += 1
+            for category in categories:
+                category_counts[category] += 1
+    return category_counts, nqueries
 
+def lookup_categories(query):
+    tokens = tokenize_query(query)
+    categories = []
+    for idx, token in enumerate(tokens):
+        if token.type == "USER_DEFINED_COMMAND":
+            categories.append("User-Defined")
+        elif token.type == "MACRO":
+            categories.append("Macro")
+        elif token.type not in ["ARGS", "PIPE", "LBRACKET", "RBRACKET"]:
+            command = token.value
+            # Note: This is an imperfect way to detect this.
+            # See queryutils.splunktypes for the right way to do it.
+            if token.value == "addtotals": 
+                if len(tokens) == idx+1:
+                    command = "addtotals row"
+                elif tokens[idx+1].value.lower()[:3] == "row":
+                    command = "addtotals row"
+                else:
+                    command = "addtotals col"
+            try:
+                categories.append(lookup_category(command))
+            except KeyError as e:
+                print e, token
+    return categories
 
-def process(querydata, commandslist):
-    categories = {}
-    missing = {}
-    for query in querydata:
-        if qhelper.validate_adhoc(query.lower()):
-            commands, missing = split_into_commands(
-                query.lower(), commandslist, missing)
-            if commands != []:
-                global total_queries
-                total_queries += 1
-                for command in commands:
-                    if command not in categories:
-                        categories[command] = 1
-                    else:
-                        categories[command] += 1
-    return categories, missing
-
-
-def split_into_commands(query, commands, missing):
-    tokens = qhelper.tokenize(query)
-    commands, missing = qhelper.split_into_commands(tokens, commands, TYPE,
-                                                    missing_commands=missing, histogram=True)
-    return commands, missing
-
-
-def convert_to_percents(categories):
-    percents = {}
-    total = float(sum(categories.values()))
-    for cat in categories:
-        count = categories[cat]
-        percents[cat] = count / total * 100
-    percents = sorted(percents.values(), reverse=True)
-    percents = ["{0:.0f}%".format(percent) for percent in percents]
-    return percents
-
-
-def create_histogram(categories):
+def create_histogram(categories, total_queries):
     names = sorted(categories, key=categories.get, reverse=True)
     percents = convert_to_percents(categories)
     counts = sorted(categories.values(), reverse=True)
@@ -78,6 +74,15 @@ def create_histogram(categories):
     plt.tight_layout()
     plt.savefig(RESULTS_DIR + "categories_histogram.png", dpi=400)
 
+def convert_to_percents(categories):
+    percents = {}
+    total = float(sum(categories.values()))
+    for category in categories:
+        count = float(categories[category])
+        percents[category] = count / total * 100.
+    percents = sorted(percents.values(), reverse=True)
+    percents = ["{0:.0f}%".format(percent) for percent in percents]
+    return percents
 
 def autolabel(rects, counts):
     for ii, rect in enumerate(rects):
@@ -87,17 +92,39 @@ def autolabel(rects, counts):
             1000, '%s' % (counts[ii]),
             ha='center', va='bottom', fontsize=20)
 
-
 def write_missing(missing):
     with open('results/nonlisted_commands.txt', 'w') as f:
         for command in sorted(missing, key=missing.get, reverse=True):
             f.write(command + ": " + str(missing[command]) + "\n")
 
+def lookup(dictionary, lookup_keys):
+    return [dictionary[k] for k in lookup_keys]
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
     parser = ArgumentParser(
-        description="Creates heatmaps for bigrams of commands mapped onto their relational type.")
-    parser.add_argument("commands", metavar="COMMANDS", type=str,
-                        help="path to list of commands used (.txt)")
+        description="Bar graph describing how frequently commands of various \
+                     types of appear in all user queries.")
+    parser.add_argument("-s", "--source",
+                        help="one of: " + ", ".join(SOURCES.keys()))
+    parser.add_argument("-a", "--path",
+                        help="the path to the data to load")
+    parser.add_argument("-v", "--version", #TODO: Print possible versions 
+                        help="the version of data collected")
+    parser.add_argument("-U", "--user",
+                        help="the user name for the Postgres database")
+    parser.add_argument("-P", "--password",
+                        help="the password for the Postgres database")
+    parser.add_argument("-D", "--database",
+                        help="the database for Postgres")
     args = parser.parse_args()
-    main(args.commands)
+    if all([arg is None for arg in vars(args).values()]):
+        parser.print_help()
+        exit()
+    if not args.source:
+        raise RuntimeError(
+            "You must specify where to fetch the data and the corresponding arguments (-s or --source).")
+    src_class = SOURCES[args.source][0]
+    src_args = lookup(vars(args), SOURCES[args.source][1])
+    source = src_class(*src_args)
+    main(source)
